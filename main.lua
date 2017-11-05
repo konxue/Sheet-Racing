f5cec101f3760e21adc541f9cda5027f81d0f795
LeonXue <konxue@gmail.com>
2017-11-05 16:52:03 -0600

    MAP

--- main.lua
+++ main.lua
@@ -7,9 +7,24 @@ local widget = require('widget')
 
   -----------------------------background--------------------------------
 
+  local optionsMap =
+  {
+    frames = {
+        { x = 59, y = 61, width = 268, height = 267}, -- Vertical tile
+        { x = 391, y = 61, width = 268, height = 267}, -- Horizontal tile
+        { x = 59, y = 385, width = 268, height = 267}, -- Start tile
+        { x = 391, y = 385, width = 268, height = 267}, -- Finish tile
+        { x = 1148, y = 61, width = 267, height = 267}, -- Left tile
+        { x = 821, y = 61, width = 268, height = 267}, -- Right tile
+        { x = 821, y = 385, width = 268, height = 267}, -- left up tile
+        { x = 1148, y = 385, width = 267, height = 267}, -- Right up tile
+    }
+  };
+  local sheetMap = graphics.newImageSheet( "map.png", optionsMap );
 
 
-
-
-
-
+  local function mapMaker ()
+  local map1 = display.newImage (sheetMap, 1, display.contentWidth/2, display.contentWidth/2);
+  local map2 = display.newImage (sheetMap, 1, display.contentWidth/2, 0);
+  end
+  mapMaker();
---
+++ map.lua
@@ -0,0 +1,15 @@
+local optionsMap =
+{
+  frames = {
+      { x = 120, y = 123, width = 530, height = 530}, -- Vertical tile
+      { x = 783, y = 123, width = 530, height = 530}, -- Horizontal tile
+      { x = 120, y = 770, width = 530, height = 530}, -- Start tile
+      { x = 783, y = 770, width = 530, height = 530}, -- Finish tile
+      { x = 2298, y = 123, width = 530, height = 530}, -- Left tile
+      { x = 1645, y = 123, width = 530, height = 530}, -- Right tile
+      { x = 1645, y = 770, width = 530, height = 530}, -- left up tile
+      { x = 2298, y = 770, width = 530, height = 530}, -- Right up tile
+  }
+};
+local sheetMap = graphics.newImageSheet( "map.png", optionsMap );
+local map1 = display.newImage (sheetMap, 1, display.contentWidth/2, display.contentWidth/2);
